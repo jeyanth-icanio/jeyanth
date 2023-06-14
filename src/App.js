c5f12d5bed3b8e682853e6578f8cf955d57b65c0
jeyanth-icanio <134058866+jeyanth-icanio@users.noreply.github.com>
2023-06-14 15:12:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          REACT APP
+         ###### REACT APP ######s
         </p>
         <h1>
         Jeyanth React App </h1>
